81be8287f997da7148e08c797a2315ca8d08a58f
Thomas Koudela <koudela@medienpark.net>
2021-01-01 12:50:32 +0100

    hello 2021

--- src/TemplateTrait.php
+++ src/TemplateTrait.php
@@ -5,7 +5,7 @@
  *
  * @package earc/native-php-template-engine
  * @link https://github.com/Koudela/eArc-native-php-template-engine
- * @copyright Copyright (c) 2020 Thomas Koudela
+ * @copyright Copyright (c) 2020-2021 Thomas Koudela
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
